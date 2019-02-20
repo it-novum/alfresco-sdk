e1b81f6bc7ddaec36c207a483b4c15816a000544
Piotr Anders <piotr.anders@it-novum.com>
2019-02-20 16:14:56 +0100

    Integrating remote and local acs deployment in a single archetype.
    * User can select if they want to use standard dockerised deployment or they prefer to start in docker DB and ASS but run ACS (with Share) in local tomcat. The selection is done using a single maven parameter (-Dlocal=true for mvn command or 'local' parameter to run scripts).
    * All docker management is done via native docker-compose but instead of executed via shell script, it is mostely executed via maven exec plugin. This allows for better integration (e.g. mvn verify works on its own). The shell scripts are adjusted for that.
    * Configuration for local tomcat is at the moment separate and stored within integration-tests module.
    * Docker compose file is split into base and override part. By default (and using default docker compose naming) it defines standard fully dockerised configuration. There is a dedicated local override file that replaces default one and defines local deployment configuration.
    * There are minor optimisations to docker-compose files and way integration tests are executed in the normal deployment mode.
    * Added solr.port parameter to supplement exsisting acs and postgres ones.
    * prerequisites element removed from main pom as it is not supported in a non-plugin project
    * local deployment mode supports JRebel but requires 'current.jrebel.agent.path' parameter set to point to JRebel native library. May be set e.g. in local settings.xml.
    * debug mode should be enabled manually via debug profile (see integration-tests module pom.xml).

--- archetypes/alfresco-allinone-archetype/src/main/resources/archetype-resources/run.bat
+++ archetypes/alfresco-allinone-archetype/src/main/resources/archetype-resources/run.bat
@@ -1,7 +1,9 @@
 #set( $symbol_dollar = '$' )
 @ECHO OFF
 
-SET COMPOSE_FILE_PATH=%CD%\target\classes\docker\docker-compose.yml
+SET COMPOSE_FILE_PATH=%CD%\target\docker\docker-compose.yml
+SET COMPOSE_FILE_OVERRIDE=%CD%\target\docker\docker-compose.override.yml
+SET MVN_BUILD_PARAMS=
 
 IF [%M2_HOME%]==[] (
     SET MVN_EXEC=mvn
@@ -12,29 +14,39 @@ IF NOT [%M2_HOME%]==[] (
 )
 
 IF [%1]==[] (
-    echo "Usage: %0 {build_start|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
-    GOTO END
+    GOTO HELP
 )
 
+IF [%2]==[local] (
+    SET COMPOSE_FILE_OVERRIDE="%CD%\target\docker\docker-compose.local.yml"
+    SET MVN_BUILD_PARAMS=-Dlocal=true %MVN_BUILD_PARAMS%
+)
+
+SET COMPOSE_FILES=-f "%COMPOSE_FILE_PATH%" -f "%COMPOSE_FILE_OVERRIDE%"
+
 IF %1==build_start (
-    CALL :down
-    CALL :build
-    CALL :start
-    CALL :tail
+    IF [%2]==[local] (
+        CALL :start_local
+    ) ELSE (
+        CALL :build
+        CALL :start_docker
+    )
     GOTO END
 )
 IF %1==start (
-    CALL :start
-    CALL :tail
+    IF [%2]==[local] (
+        CALL :start_local
+    ) ELSE (
+        CALL :start_docker
+    )
     GOTO END
 )
 IF %1==stop (
-    CALL :down
+    CALL :down_docker
     GOTO END
 )
 IF %1==purge (
-    CALL:down
-    CALL:purge
+    CALL :purge
     GOTO END
 )
 IF %1==tail (
@@ -42,79 +54,65 @@ IF %1==tail (
     GOTO END
 )
 IF %1==reload_share (
-    CALL :build_share
-    CALL :start_share
-    CALL :tail
+    IF [%2]==[local] GOTO HELP
+    CALL :reload_share
     GOTO END
 )
 IF %1==reload_acs (
-    CALL :build_acs
-    CALL :start_acs
-    CALL :tail
+    IF [%2]==[local] GOTO HELP
+    CALL :reload_acs
     GOTO END
 )
 IF %1==build_test (
-    CALL :down
-    CALL :build
-    CALL :prepare-test
-    CALL :start
     CALL :test
     CALL :tail_all
-    CALL :down
+    CALL :down_docker
     GOTO END
 )
 IF %1==test (
     CALL :test
     GOTO END
 )
-echo "Usage: %0 {build_start|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
+:HELP
+echo "Usage: %0 {build_start|start|stop|purge|tail|reload_share|reload_acs|build_test|test} [local]"
+echo "Or   : %0 {build_start|start|stop|purge|tail|build_test|test} local"
 :END
 EXIT /B %ERRORLEVEL%
 
-:start
-    docker volume create ${rootArtifactId}-acs-volume
-    docker volume create ${rootArtifactId}-db-volume
-    docker volume create ${rootArtifactId}-ass-volume
-    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d
+:start_docker
+    call %MVN_EXEC% package exec:exec@start-docker exec:exec@tail-docker -N %MVN_BUILD_PARAMS%
 EXIT /B 0
-:start_share
-    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d ${rootArtifactId}-share
+:start_local
+    call %MVN_EXEC% package exec:exec@start-docker cargo:run %MVN_BUILD_PARAMS%
 EXIT /B 0
-:start_acs
-    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d ${rootArtifactId}-acs
-EXIT /B 0
-:down
-    if exist "%COMPOSE_FILE_PATH%" (
-        docker-compose -f "%COMPOSE_FILE_PATH%" down
-    )
+:down_docker
+    call %MVN_EXEC% package exec:exec@stop-docker -N %MVN_BUILD_PARAMS%
 EXIT /B 0
 :build
-	call %MVN_EXEC% clean package
+    call %MVN_EXEC% package %MVN_BUILD_PARAMS%
 EXIT /B 0
-:build_share
-    docker-compose -f "%COMPOSE_FILE_PATH%" kill ${rootArtifactId}-share
-    docker-compose -f "%COMPOSE_FILE_PATH%" rm -f ${rootArtifactId}-share
-	call %MVN_EXEC% clean package -pl ${rootArtifactId}-share,${rootArtifactId}-share-docker
+:reload_share
+    docker-compose %COMPOSE_FILES% kill ${rootArtifactId}-share
+    docker-compose %COMPOSE_FILES% rm -f ${rootArtifactId}-share
+    call %MVN_EXEC% package -pl ${rootArtifactId}-share,${rootArtifactId}-share-docker
+    docker-compose %COMPOSE_FILES% up --build -d ${rootArtifactId}-share
 EXIT /B 0
-:build_acs
-    docker-compose -f "%COMPOSE_FILE_PATH%" kill ${rootArtifactId}-acs
-    docker-compose -f "%COMPOSE_FILE_PATH%" rm -f ${rootArtifactId}-acs
-	call %MVN_EXEC% clean package -pl ${rootArtifactId}-platform,${rootArtifactId}-platform-docker
+:reload_acs
+    docker-compose %COMPOSE_FILES%" kill ${rootArtifactId}-acs
+    docker-compose %COMPOSE_FILES%" rm -f ${rootArtifactId}-acs
+    call %MVN_EXEC% package -pl ${rootArtifactId}-platform,${rootArtifactId}-platform-docker
+    docker-compose %COMPOSE_FILES% up --build -d ${rootArtifactId}-acs
 EXIT /B 0
 :tail
-    docker-compose -f "%COMPOSE_FILE_PATH%" logs -f
+    call %MVN_EXEC% package exec:exec@tail-docker -N %MVN_BUILD_PARAMS%
 EXIT /B 0
 :tail_all
-    docker-compose -f "%COMPOSE_FILE_PATH%" logs --tail="all"
-EXIT /B 0
-:prepare-test
-    call %MVN_EXEC% verify -DskipTests=true -pl ${rootArtifactId}-platform,${rootArtifactId}-integration-tests,${rootArtifactId}-platform-docker
+    call %MVN_EXEC% package exec:exec@tail-all-docker -N %MVN_BUILD_PARAMS%
 EXIT /B 0
 :test
-    call %MVN_EXEC% verify -pl ${rootArtifactId}-platform,${rootArtifactId}-integration-tests
+    call %MVN_EXEC% verify %MVN_BUILD_PARAMS%
 EXIT /B 0
 :purge
-    docker volume rm -f ${rootArtifactId}-acs-volume
-    docker volume rm -f ${rootArtifactId}-db-volume
-    docker volume rm -f ${rootArtifactId}-ass-volume
+    call %MVN_EXEC% package exec:exec@purge-docker -N %MVN_BUILD_PARAMS%
+    call %MVN_EXEC% clean %MVN_BUILD_PARAMS%
 EXIT /B 0
--- archetypes/alfresco-allinone-archetype/src/main/resources/archetype-resources/run.sh
+++ archetypes/alfresco-allinone-archetype/src/main/resources/archetype-resources/run.sh
@@ -2,6 +2,8 @@
 #!/bin/sh
 
 export COMPOSE_FILE_PATH=${symbol_dollar}{PWD}/target/classes/docker/docker-compose.yml
+export COMPOSE_FILE_OVERRIDE=${symbol_dollar}{PWD}/target/classes/docker/docker-compose.override.yml
+export MVN_BUILD_PARAMS=
 
 if [ -z "${symbol_dollar}{M2_HOME}" ]; then
   export MVN_EXEC="mvn"
@@ -9,108 +11,110 @@ else
   export MVN_EXEC="${symbol_dollar}{M2_HOME}/bin/mvn"
 fi
 
-start() {
-    docker volume create ${rootArtifactId}-acs-volume
-    docker volume create ${rootArtifactId}-db-volume
-    docker volume create ${rootArtifactId}-ass-volume
-    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH up --build -d
+if [ "$2"=="local" ]; then
+  export COMPOSE_FILE_OVERRIDE=${symbol_dollar}{PWD}/target/classes/docker/docker-compose.local.yml
+  export MVN_BUILD_PARAMS=-Dlocal=true ${symbol_dollar}{MVN_BUILD_PARAMS}
+fi
+
+start_docker() {
+    ${symbol_dollar}MVN_EXEC package exec:exec@start-docker exec:exec@tail-docker -N ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
-start_share() {
-    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH up --build -d ${rootArtifactId}-share
+start_local() {
+    ${symbol_dollar}MVN_EXEC package exec:exec@start-docker cargo:run ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
-start_acs() {
-    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH up --build -d ${rootArtifactId}-acs
-}
-
-down() {
-    if [ -f ${symbol_dollar}COMPOSE_FILE_PATH ]; then
-        docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH down
-    fi
+down_docker() {
+    ${symbol_dollar}MVN_EXEC package exec:exec@stop-docker -N ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
 purge() {
-    docker volume rm -f ${rootArtifactId}-acs-volume
-    docker volume rm -f ${rootArtifactId}-db-volume
-    docker volume rm -f ${rootArtifactId}-ass-volume
+    ${symbol_dollar}MVN_EXEC package exec:exec@purge-docker -N ${symbol_dollar}{MVN_BUILD_PARAMS}
+    ${symbol_dollar}MVN_EXEC clean ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
 build() {
-    ${symbol_dollar}MVN_EXEC clean package
+    ${symbol_dollar}MVN_EXEC package ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
-build_share() {
+reload_share() {
     docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH kill ${rootArtifactId}-share
     yes | docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH rm -f ${rootArtifactId}-share
     ${symbol_dollar}MVN_EXEC clean package -pl ${rootArtifactId}-share,${rootArtifactId}-share-docker
+    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH up --build -d ${rootArtifactId}-share
 }
 
-build_acs() {
+reload_acs() {
     docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH kill ${rootArtifactId}-acs
     yes | docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH rm -f ${rootArtifactId}-acs
     ${symbol_dollar}MVN_EXEC clean package -pl ${rootArtifactId}-platform,${rootArtifactId}-platform-docker
+    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH up --build -d ${rootArtifactId}-acs
 }
 
 tail() {
-    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH logs -f
+    ${symbol_dollar}MVN_EXEC package exec:exec@tail-docker -N ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
 tail_all() {
-    docker-compose -f ${symbol_dollar}COMPOSE_FILE_PATH logs --tail="all"
-}
-
-prepare_test() {
-    ${symbol_dollar}MVN_EXEC verify -DskipTests=true -pl ${rootArtifactId}-platform,${rootArtifactId}-integration-tests,${rootArtifactId}-platform-docker
+    ${symbol_dollar}MVN_EXEC package exec:exec@tail-all-docker -N ${symbol_dollar}{MVN_BUILD_PARAMS}
 }
 
 test() {
-    ${symbol_dollar}MVN_EXEC verify -pl ${rootArtifactId}-platform,${rootArtifactId}-integration-tests
+    ${symbol_dollar}MVN_EXEC verify ${symbol_dollar}{MVN_BUILD_PARAMS}
+}
+
+help() {
+    echo "Usage: ${symbol_dollar}0 {build_start|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
+    echo "Or   : ${symbol_dollar}0 {build_start|start|stop|purge|tail|build_test|test} local"
 }
 
 case "${symbol_dollar}1" in
   build_start)
-    down
-    build
-    start
-    tail
+    if [ "$2"=="local" ]; then
+        start_local
+    else
+        build
+        start_docker
+    fi
     ;;
   start)
-    start
-    tail
+    if [ "$2"=="local" ]; then
+        start_local
+    else
+        start_docker
+    fi
     ;;
   stop)
-    down
+    down_docker
     ;;
   purge)
-    down
     purge
     ;;
   tail)
     tail
     ;;
   reload_share)
-    build_share
-    start_share
-    tail
+    if [ "$2"=="local" ]; then
+        help
+    else
+        reload_share
+    fi
     ;;
   reload_acs)
-    build_acs
-    start_acs
-    tail
+    if [ "$2"=="local" ]; then
+        help
+    else
+        reload_acs
+    fi
     ;;
   build_test)
-    down
-    build
-    prepare_test
-    start
     test
     tail_all
-    down
+    down_docker
     ;;
   test)
     test
     ;;
   *)
-    echo "Usage: ${symbol_dollar}0 {build_start|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
+    help
 esac
